b39373fad6e0ae01b251c1a142f25b5e9d57b95b
Bianca161 <biancaholandadejesus>
2026-06-02 16:50:56 -0300

    implementa funcionalidades do carrinho e catálogo de produtos

--- src/components/ products/ProductList.vue
+++ src/components/ products/ProductList.vue
@@ -1,8 +1,19 @@
 <script setup>
+import ProductCard from './ProductCard.vue'
+import {products} from '../../data/products.js'
 </script>
 
 <template>
-  <h1>Lista de Produtos</h1>
+  <h1>Catálogo de Livros</h1>
+
+  <div class="product-list">
+    <ProductCard 
+      v-for="item in products" 
+      :key="item.id" 
+      :product="item"
+      @add-to-cart="$emit('add-to-cart', item)"
+    />
+  </div>
 </template>
 
 <style scoped>
--- src/components/cart/CartPanel.vue
+++ src/components/cart/CartPanel.vue
@@ -1,9 +1,54 @@
 <script setup>
+import CartItem from './CartItem.vue'
+import CartSummary from './CartSummary.vue'
+
+const props = defineProps({
+  cartItems: {
+    type: Array,
+    required: true,
+  },
+  cartTotal: {
+    type: Number,
+    required: true,
+  },
+})
 </script>
 
 <template>
   <h1>Painel do Carrinho</h1>
+
+  <div class="cart-page">
+    <h1 class="cart-title">Carrinho de Compras</h1>
+
+    <div class="cart-container">
+      <div class="cart-main">
+        <div class="cart-header">
+          <span class="header-titulo">Título</span>
+          <span class="header-quantidade">Quantidade</span>
+          <span class="header-subtotal">Subtotal</span>
+        </div>
+
+        <div class="cart-items">
+          <CartItem
+            v-for="item in cartItems"
+            :key="item.id"
+            :item="item"
+            @increase-qty="$emit('increase-qty', item)"
+            @decrease-qty="$emit('decrease-qty', item)"
+          />
+        </div>
+
+        <button class="btn-back">Voltar para loja</button>
+
+        <div class="coupon-section">
+          <input type="text" placeholder="Código do cupom" class="coupon-input" />
+          <button class="btn-coupon">Inserir Cupom</button>
+        </div>
+      </div>
+
+      <CartSummary :totalProdutos="cartTotal" />
+    </div>
+  </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
