bd3fe9af7988694e323dccdcb4018b4047610ee9
saraSchulz <sara.ifc2024@gmail.com>
2026-06-02 15:00:19 -0300

    configurações de pagina

---
+++ src/data/products.js
@@ -0,0 +1,58 @@
+const books = [
+    {
+        id: 1,
+        image: '/booksImgs/domCasmurro.png',
+        title: 'Dom Casmurro',
+        autor: 'Machado de Assis',
+        preco: 24.90
+    },
+    {
+        id: 2,
+        image: '/booksImgs/vidasSecas.png',
+        title: 'Vidas Secas',
+        autor: 'Graciliano Ramos',
+        preco: 19.90
+    },
+    {
+        id: 3,
+        image: '/booksImgs/macunaima.png',
+        title: 'Macunaíma',
+        autor: 'Mário de Andrade',
+        preco: 22.80
+    },
+    {
+        id: 4,
+        image: '/booksImgs/tristeQuaresma.png',
+        title: 'Triste Fim de Policarpo Quaresma',
+        autor: 'Lima Barreto',
+        preco: 23.10
+    },
+    {
+        id: 5,
+        image: '/booksImgs/memoriasCubas.png',
+        title: 'Memórias Póstumas de Brás Cubas',
+        autor: 'Machado de Assis',
+        preco: 21.50
+    },
+    {
+        id: 6,
+        image: '/booksImgs/capitaesAreia.png',
+        title: 'Capitães da Areia',
+        autor: 'Jorge Amado',
+        preco: 18.75
+    },
+    {
+        id: 7,
+        image: '/booksImgs/iracema.png',
+        title: 'Iracema',
+        autor: 'José de Alencar',
+        preco: 17.40
+    },
+    {
+        id: 8,
+        image: '/booksImgs/moreninha.png',
+        title: 'A Moreninha',
+        autor: 'Joaquim Manuel de Macedo',
+        preco: 16.95
+    }
+]
