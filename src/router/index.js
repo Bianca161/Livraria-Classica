1e6095e37f06fa3e6970f15114e5cab99b151d39
saraSchulz-art <sara.ifc2024@gmail.com>
2026-06-09 20:21:40 -0300

    remove linha router

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '@/components/layout/HomeView.vue'
-<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+
 
 const routes = [
   {
