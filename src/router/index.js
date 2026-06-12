54eaf0d1f5b96446c9334f448ba1bc3b9c3a881d
mizaelestevam <vambastter14@gmail.com>
2026-06-12 14:20:08 -0300

    a

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,37 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '@/components/layout/HomeView.vue'
-<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '@/components/layout/HomeView.vue';
+import TeamView from '@/components/layout/TeamView.vue';
+import BookDetailView from '@/components/layout/BookDetailView.vue';
+import CartPanel from '@/components/cart/CartPanel.vue';
+import ProductList from '@/components/products/ProductList.vue';
+
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: HomeView,
+  },
+  {
+    path: '/equipe',
+    name: 'Team',
+    component: TeamView,
+  },
+  {
+    path: '/livro/:id',
+    name: 'book-detail',
+    component: BookDetailView,
+  },
+  {
+    path: '/carrinho',
+    name: 'Cart',
+    component: ProductList,
+  }
+];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-  ],
-})
+  history: createWebHistory(),
+  routes,
+});
 
-export default router
+export default router;
